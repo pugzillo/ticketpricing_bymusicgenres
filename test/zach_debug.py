a7f9698b9f25c30ea80ffddb3fac832f8a469bcf
Linh <lcahu91@gmail.com>
2019-10-14 10:09:10 -0700

    first commit

---
+++ test/zach_debug.py
@@ -0,0 +1,7 @@
+# ticket_concert_artist.csv
+# spotify_api_musicfeatures.py
+with open('ticket_concert_artists.csv') as f:
+    rows = sorted(row for row in f)
+print(rows[0])
+
+name = '!!! (Chk Chk Chk)'
