0ae25f852b8f7bdc106d55a34d3219752c2293fd
Linh <lcahu91@gmail.com>
2019-10-15 13:20:33 -0700

    Removing ignored files

--- stubhub_api_token.bash
+++
@@ -1,4 +0,0 @@
-curl -X POST --header "Authorization: Basic base64encodedValue​" --header "Content-Type: application/json" -d '{
-   "username": "lchau91@gmail.com",
-   "password": "5m*8R0WgjQ*y"
-}' https://api.stubhub.com/sellers/oauth/accesstoken?grant_type=client_credentials
